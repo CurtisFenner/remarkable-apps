0dcc8a6e3120d8c0e44fb36a0853f8142e5962de
Curtis Fenner <3289892+CurtisFenner@users.noreply.github.com>
2021-02-10 12:34:28 -0500

    Update build.py

--- build.py
+++ build.py
@@ -3,45 +3,41 @@ import os
 import subprocess
 
 LUA_MAINS = set(["lua.c", "luac.c"])
-
-# Build Lua 5.3.6.
-for c_path in glob.glob("../lua-5.3.6/src/*.c"):
-    c_directory, c_file = os.path.split(c_path)
-    target = "built/luas/" + c_file + ".o"
-    if c_file not in LUA_MAINS and not os.path.exists(target):
-        subprocess.run(["arm-linux-gnueabihf-gcc"]
-                       + ["-Wall", "-Wextra",  "-O3"]
-                       + ["-c", c_path]
-                       + ["-o", target]
-                       + ["-lm"])
-
-# Build Lua into a static library.
-# For some reason, Lua is depending on glibc@2.29 for math functions
-# (cos, atan2, etc), but the reMarkable only has 2.27.
-# Statically linking it covers up this problem.
-subprocess.run(["arm-linux-gnueabihf-ar", "rcs"]
-               + ["built/luas/all.a"]
-               + glob.glob("built/luas/*.o"))
-
-# Build the app.
-flags = ["-Wall", "-Wextra", "-O3", "-flto"]
 source_files = ["framebuffer.c", "input.c", "main.c", "interpreter.c"]
 intermediates = ["built/luas/all.a"]
-libraries = ["dl", "rt", "m"]
+libraries = ["dl", "rt"]
 exe = "built/curtis"
+
+################################################################################
+
+# Build Lua 5.3.6.
+# Build Lua into a static library.
+if not os.path.exists("built/luas/all.a"):
+    for c_path in glob.glob("../lua-5.3.6/src/*.c"):
+        c_directory, c_file = os.path.split(c_path)
+        target = "built/luas/" + c_file + ".o"
+        if c_file not in LUA_MAINS and not os.path.exists(target):
+            subprocess.run(["arm-linux-gnueabihf-gcc"]
+                           + ["-Wall", "-Wextra",  "-O3"]
+                           + ["-c", c_path]
+                           + ["-o", target]
+                           + ["-lm"])
+    subprocess.run(["arm-linux-gnueabihf-ar", "rcs"]
+                   + ["built/luas/all.a"]
+                   + glob.glob("built/luas/*.o"))
+
 command = (["arm-linux-gnueabihf-gcc"]
-           + flags
            + ["-o", exe]
-           + ["-Wl,-L", "-Wl,../rmlib/lib"]
            + ["-I../lua-5.3.6/src"]
            + source_files
-           + glob.glob("../rmlib/lib/*.so")
            + intermediates
+           # N.B.: The GCC version between my toolchain and the reMarkable
+           # differs, so instead provide the reMarkable's libm.
+           # This must FOLLOW intermediates, because ld only uses a .so if
+           # previous arguments referenced the symbol.
+           + ["../rmlib/lib/libm.so.6"]
            + ["-l" + lib for lib in libraries])
-
-print("\t".join(command))
 subprocess.run(command)
 
 # Send the compiled ELF to the reMarkable 2.
-remote = "root@remarkable"
-subprocess.run(["scp", exe, remote + ":/home/root/curtis"])
+subprocess.run(["scp", exe, "root@remarkable:/home/root/curtis"])
--- compile.sh
+++
@@ -1,4 +0,0 @@
-arm-linux-gnueabihf-gcc -Wall -Wextra -o "built/$1" -I/usr/arm-linux-gnueabihf/include/ \
-	framebuffer.c main.c \
-	-ldl -lrt -lm \
-	 && scp "built/$1" "root@192.168.1.105:/home/root/$1"
